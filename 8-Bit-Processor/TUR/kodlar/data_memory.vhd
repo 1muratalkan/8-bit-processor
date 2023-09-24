5986da18e14a5aebec05fc1c524aae3922bd6ba4
MURAT ALKAN <108190826+1muratalkan@users.noreply.github.com>
2023-09-24 21:55:59 +0300

    Update data_memory.vhd

--- 8-Bit-Processor/TUR/kodlar/data_memory.vhd
+++ 8-Bit-Processor/TUR/kodlar/data_memory.vhd
@@ -18,7 +18,7 @@ end entity;
 architecture arch of data_memory is
 
 type ram_type is array (128 to 223) of std_logic_vector( 7 downto 0); -- 96x8-bit
-signal RAM : ram_type := (others => "00"); -- Baslangic verileri hepsi sifir
+signal RAM : ram_type := (others => x"00"); -- Baslangic verileri hepsi sifir
  
 signal enable : std_logic;
 
